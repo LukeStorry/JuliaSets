c598b7b3987777f72a400b284f7ff898af68f8e3
LukeStorry <luke@lukestorry.co.uk>
2014-10-10 13:40:55 +0100

    Update Output 2D array.py

--- Python/Output 2D array.py
+++ Python/Output 2D array.py
@@ -6,22 +6,26 @@ resolution=10
 sizeLimit=100
 iterationsLimit=100
 
-#Main Code starts here
-def PlotJulia():              		# Overall Function to make a 2D array and fill it
-  outputTable = []					# Initializes the list.
-  for i in range(0,resolution):			# Make a list of rows, to the resolution wanted.
-    outputTable.append([])
-    for j in range(0,resolution):		# In each row list, make a list of columns.
-      outputTable[i].append(findNumber(i,j))	# fill each cell with a value
-  return outputTable
-      
 def findNumber(i,j):				# function to find how many iterations before the number explodes. 
-  iterations=0					# 
-  x=xMin+j*(xMax-xMin)/resolution		# transform array j coordinate to complex plane x coordinates?
-  y=yMax-i*(yMax-yMin)/resolution		# transform array i coordinate to complex plane y coordinate
-  while (abs(x)<sizeLimit) and (abs(y)<sizeLimit) and (iterations<iterationsLimit): 
-	x,y=(x*x-y*y+juliaC[0]),(2*x*y+juliaC[1])# apply function
-	iterations=iterations+1			# increment iterations counter
-  return iterations				# output as function value
+    iterations=0					# 
+    x=xMin+j*(xMax-xMin)/resolution		# transform array j coordinate to complex plane x coordinates?
+    y=yMax-i*(yMax-yMin)/resolution		# transform array i coordinate to complex plane y coordinate
+    while (abs(x)<sizeLimit) and (abs(y)<sizeLimit) and (iterations<iterationsLimit): 
+	    (x,y)=(x*x-y*y+juliaC[0]),(2*x*y+juliaC[1])# apply function
+	    iterations++    			# increment iterations counter
+    return iterations				# output as function value
 
-print(PlotJulia())
+def PlotJulia():              		# Overall Function to make a 2D array and fill it
+    outputTable = []					# Initializes the list.
+    for i in range(0,resolution):			# Make a list of rows, to the resolution wanted.
+        outputTable.append([])
+        for j in range(0,resolution):		# In each row list, make a list of columns.
+            outputTable[i].append(findNumber(i,j))	# fill each cell with a value
+    return outputTable
+
+def OutputArray():					# Initializes the list.
+    for i in range(0,resolution):			# In each row,.
+        for j in range(0,resolution):		# In each cell, 
+            print("%3.2f ", outputTable[i][j] #output cell
+        print("\n")     #then newline
+        
