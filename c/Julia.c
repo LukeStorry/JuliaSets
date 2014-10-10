98c76d3017bc0450d5d43db9374515c5ee3fef06
LukeStorry <luke@lukestorry.co.uk>
2014-10-10 13:27:55 +0100

    Create Julia.c

---
+++ c/Julia.c
@@ -0,0 +1,33 @@
+#define
+#define
+#define
+#define
+#define
+#define
+#define
+
+
+
+#include<stdio.h>
+
+//declare Complex Type?
+
+//Is there a library for complexes?
+
+Complex SquareNumbers(float comp1, float comp2) {
+    return(comp1.i+comp2.j ............... )
+};
+
+array? CalculateJuliaSet(float c){
+
+};
+
+
+int output(array?) {
+
+};
+
+int main(void) {
+
+    return 0;
+}
