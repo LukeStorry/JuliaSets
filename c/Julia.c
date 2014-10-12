de90d1c803e69c3605f4f236d843cb8e88bed4ad
LukeStorry <luke@lukestorry.co.uk>
2014-10-12 21:27:58 +0100

    fixed some bugs

--- c/Julia.c
+++ c/Julia.c
@@ -18,60 +18,58 @@ const resY = 10;
 const maxIts = 100;
 const complex julC = {0,1};
 
-complex Transform(int i,j) {
+complex transform(int i, int j) {
     complex output;
     output.x = 1/*calculate the algorithm what goes here*/;
     output.y = 1/*here too*/;
     return output;
 };
 
-short ExceededMax(int input){
+short exceededMax(int input){
     if (input>maxIts) {return 1;}
         else {return 0;};
 };
 
-short Escaped(complex input){
+short escaped(complex input){
     if (input.x>ESCX||input.y>ESCY) {return 1;}
         else {return 0;};
 };
 
-complex Iterate(complex input) {
+complex iterate(complex input) {
     complex output;
     output.x=input.x*input.x-input.y*input.y +julC.x;
     output.y=2*input.x*input.y +julC.y;
     return output;
 };
 
-int FindValue(int i,j){
-    complex point=Transform(i,j);
+int findValue(int i, int j){
+    complex point=transform(i,j);
     int iterations=1;
-    while( (! ExceededMax(iterations)) && (! Escaped(point))){
-        Iterate(point);
+    while( (! exceededMax(iterations)) && (! escaped(point))){
+        iterate(point);
         iterations++;
     };
     return iterations;
 };
 
 
-void CalcJuliaSet(int *start) {
+void calcJuliaSet(int *start) {
     int i,j;
-    for(i=0;i<resX;i++){
-        for(j=0;j<resY;j++){
-           // complex input = {i,j};
-            start+(resX*i)+j=FindValue(i,j);
+    for(i=0;i<(resX);i++){
+        for(j=0;j<(resY);j++){
+            *(start+(i*resX)+j)=findValue(i,j);
         };
-     };
-    return 0;
+    };
 };
 
-void Output(int* input){
+void output(int* input){
     ;
 };
 
-int Main(void) {
+int main(void) {
     int table[resX*resY];//allocates memory space for the table
-    CalcJuliaSet(&table);
-    Output(&table);
+    calcJuliaSet(table);
+    output(table);
     return 0;
 }
 
