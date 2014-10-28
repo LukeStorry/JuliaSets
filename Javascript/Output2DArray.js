0a1d1abdf5e3ac4bc5157bda3a6c51f6ae9c72a9
Luke Storry <luke@lukestorry.co.uk>
2014-10-28 14:35:27 +0000

    Update Output2DArray.js

--- Javascript/Output2DArray.js
+++ Javascript/Output2DArray.js
@@ -1,12 +1,26 @@
  //Global Variables. Can ask for input in later versions
 var juliaC = [0,0];
-var xMin = -2.1;
-var xMax = 2;
-var yMin = -2.1;
-var yMax = 2;
-var resolution = 10;
-var sizeLimit=100;
-var iterLimit=100;
+var xMin = -1.8;
+var xMax = 1.8;
+var yMin = -1.4;
+var yMax = 1.4;
+var resolution = 20;
+var iterLimit= 99;
+
+
+var findNumber = function(i,j) {				//function to find how many iterations before the number explodes. 
+  var a, b, iterations = 0;					
+  x = xMin+j*(xMax-xMin)/resolution;             //transform array j coordinate to complex plane x coordinates?
+  y = yMax-i*(yMax-yMin)/resolution;	             // transform array i coordinate to complex plane y coordinate
+  while (Math.abs(x+juliaC[0])<1.5 && Math.abs(y+juliaC[1])<1.5 && iterations<iterLimit) { 
+    a = x*x-y*y+juliaC[0];                   //apply function to temp variables
+    b = 2*x*y+juliaC[1];	
+    x = a;                                       //reassign values to x&y
+    y = b;
+    iterations = iterations + 1;                                //increment iterations counter
+  }; 
+  return iterations;                             //output as function value
+};
 
 var findArray = function() {                    //Overall Function to make a 2D array and fill it
   var outputTable = [];                         //Initializes the array
@@ -14,22 +28,9 @@ var findArray = function() {                    //Overall Function to make a 2D 
     outputTable[i] = [];                        //Initializes the array within each array value
     for (var j=0 ; j < resolution ; j++) {	   	//In each row list, make a list of columns.
       outputTable[i][j] = findNumber(i,j);      //fill each cell with a value
-  } };
+    }
+  };
   return outputTable;
-};
-
-var findNumber = function(i,j) {				//function to find how many iterations before the number explodes. 
-  var iterations = 0;					
-  x = xMin+j*(xMax-xMin)/resolution;             //transform array j coordinate to complex plane x coordinates?
-  y = yMax-i*(yMax-yMin)/resolution;	             // transform array i coordinate to complex plane y coordinate
-  while (Math.abs(x)<sizeLimit && Math.abs(y)<sizeLimit && iterations<iterLimit) { 
-	  var a = x*x-y*y+juliaC[0];                   //apply function to temp variables
-    var b = 2*x*y+juliaC[1];	
-    x = a;                                       //reassign values to x&y
-    y = b;
-   iterations = iterations + 1;                                //increment iterations counter
-  } 
-  return iterations;                             //output as function value
 };
 
 console.log(findArray());
