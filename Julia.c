42a6372c4670f6d6c18da867aa89461c850a720f
LukeStorry <luke@lukestorry.co.uk>
2014-11-05 12:58:30 +0000

    changing global variables to local and pointer

--- Julia.c
+++ Julia.c
@@ -12,36 +12,38 @@ typedef struct {							//defined a new datatype to ease the handling of complex 
 
 
 //default settings:
-    double  minX = -1.8;
-    double  maxX = 1.8;
-    double  minY = -1.44;
-    double  maxY = 1.44;
-    long    resX = 1280;
-    long    resY = 1024;
-    unsigned  maxIts = 254;
-    double  escmax = 50;
-    complex julC = {-1,0};
-    char filepath[100] = "output.ppm";
+typedef struct {
+    double  minX;
+    double  maxX;
+    double  minY;
+    double  maxY;
+    long    resX;
+    long    resY;
+    unsigned  maxIts;
+    double  escMax;
+    complex julC;
+    char filepath[100];
+}settings;
 
-
-int fixSettings(int argc, char** argv) {
+int fixSettings(int argc, char** argv, settings*s) {
     switch (argc) {
 	case 1:
-	    break;
+	    (*s) = (settings){-1.5,1.5,-1.5,1.5,200,200,255, 50,(complex){-1,0},"output.ppm"}; //default settings
 	case 2:
-	    strcpy(filepath,argv[1]);
+	    (*s) = (settings){-1.5,1.5,-1.5,1.5,200,200,255, 50,(complex){-1,0},""}; //defaults
+	    strcpy((*s).filepath,argv[1]);
 	    break;
 	case NOOFSETTINGS:
-	    minX = atof(argv[1]);
-	    maxX = atof(argv[2]);
-	    minY = atof(argv[3]);
-	    maxY = atof(argv[4]);
-	    resX = atof(argv[5]);
-	    resY = atof(argv[6]);	
-	    maxIts = atof(argv[7]);
-	    julC.x = atof(argv[8]);
-	    julC.y = atof(argv[9]);
-	    strcpy(filepath,argv[10]);
+	    (*s).minX = atof(argv[1]);
+	    (*s).maxX = atof(argv[2]);
+	    (*s).minY = atof(argv[3]);
+	    (*s).maxY = atof(argv[4]);
+	    (*s).resX = atof(argv[5]);
+	    (*s).resY = atof(argv[6]);	
+	    (*s).maxIts = atof(argv[7]);
+	    (*s).julC.x = atof(argv[8]);
+	    (*s).julC.y = atof(argv[9]);
+	    strcpy((*s).filepath,argv[10]);
 	    break;
 	default:
 	    return 0;						
@@ -50,7 +52,7 @@ int fixSettings(int argc, char** argv) {
 };
 
 
-int exceededMax(unsigned int input) {					//this function tests whether iterations has hit the maximum allwed
+int exceededMax(unsigned int input, unsigned maxIts) {					//this function tests whether iterations has hit the maximum allwed
     if (input > maxIts) {						//if the current number of iterations is higher than the max allowed,
         return 1;							//then return true
     } else {								//otherwise, when the iterations have not yet reached maximum,
@@ -59,8 +61,8 @@ int exceededMax(unsigned int input) {					//this function tests whether iteratio
 };
 
 
-int escaped(complex point) {					//This function tests for if the point has escaped and will not return
-    if ( (abs(point.x + julC.x) > escmax) || (abs(point.y + julC.y) > escmax)){	//If the point is big,
+int escaped(complex point, escMax) {					//This function tests for if the point has escaped and will not return
+    if ( (abs(point.x + julC.x) > escMax) || (abs(point.y + julC.y) > escMax)){	//If the point is big,
         return 1;							//then return true
     } else {								//else, when the point is small
         return 0;							//return false
@@ -76,10 +78,10 @@ complex iterate(complex input) {    					//This function performs a function upo
 };
 
 
-long findValue(double x, double y) {			//this function find the value with which to populate each cell
+long findValue(double x, double y, settings * s) {			//this function find the value with which to populate each cell
     complex point = {x,y};					//translates the (i,j) array coordinates into a (x,y) complex number
     unsigned int iterations = 0;					//declares the iteration counter
-    while( ! exceededMax(iterations) && ! escaped(point) ) {		//while the point hasn't escaped, or iterations almost hit infinity
+    while( ! exceededMax(iterations,s.maxIts)) && ! escaped(point,s.escMax) ) {		//while the point hasn't escaped, or iterations almost hit infinity
         point = iterate(point);						//perform the function on the point
         iterations++;							//increment the diagnostics counter
     };
@@ -87,29 +89,30 @@ long findValue(double x, double y) {			//this function find the value with which
 };
 
 
-void fillPPM(FILE* file) {
+void fillPPM(FILE*file, settings*s) {
     double x,y;
     for(y=maxY ; y>=minY ; y-=((maxY-minY)/resY)){						//for every row,
 	for(x=minX ; x<=maxX ; x+=((maxX-minX)/resX)){   					//for each cell,
-             fprintf(file,"%i %i %li ",0,0,findValue(x,y));
+             fprintf(file,"%i %i %li ",0,0,findValue(x,y,s));
         };
     };
 };
 
 //using http://www.physics.emory.edu/faculty/weeks//graphics/mkppm.html
-void createPPM() {
+void createPPM(settings * s) {
     FILE* file = fopen(filepath,"w");
     fprintf(file,"P3\n");
     fprintf(file,"#A Julia set image, generated in C by Luke Storry\n");
-    fprintf(file,"%lu %lu\n%d\n", resX, resY,255);
-    fillPPM(file);
+    fprintf(file,"%lu %lu\n%d\n", s.resX, s.resY, 255);
+    fillPPM(file,s);
     fclose(file);
 };
 
 
 int main(int argc, char** argv) {					//main function. it all starts here.
-    if (fixSettings(argc,argv)==1){
-      	createPPM();							//prints the array
+    settings s;
+    if (fixSettings(argc,argv,&s)==1){
+      	createPPM(s);							//prints the array
 	return 0;
     } else {
 	printf("Incorrect Parameters\n");
