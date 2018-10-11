d057eddf2771cb3a2c514b06ccbdf6f87953d1be
Luke Storry <luke@lukestorry.co.uk>
2018-10-11 12:29:15 +0100

    produced bigger & better gifs

--- juliaset_gif.py
+++ juliaset_gif.py
@@ -2,45 +2,52 @@
 
 import numpy as np
 import imageio
+import time
 
-
-def julia_grid(c_a, c_b, width, height, max_iterations=100):
+def julia_grid(c_a, c_b, width, height, max_iterations=50):
     """Generates a numpy array of ints out of 255, representing iterations until explosion"""
     c = complex(c_a, c_b)
-    xscale = 3 / width
+    xscale = 4 / width
     yscale = xscale
 
-    grid = np.zeros((width, height))
+    grid = np.zeros((height, width))
     for a in range(width):
         for b in range(height):
             z = complex(-xscale * width / 2 + a * xscale,
                         -yscale * height / 2 + b * yscale)
 
             iterations = 0
-            while abs(z) <= xscale * width * 10 and iterations < max_iterations:
+            while abs(z) <= xscale * width  and iterations < max_iterations:
                 z = z**2 + c
                 iterations += 1
 
-            grid[a, b] = iterations / max_iterations * 255
+            grid[b, a] = iterations / max_iterations * 255
 
     #grid = grid.repeat(4,axis=0).repeat(4,axis=1)
     return grid
 
 
-def create_gif(width=800, height=800):
+def create_gif(width=1200, height=1000):
     julias = []
-    for c_a in (x/5 for x in range(-7,2)):
+    forwards = True
+
+    for c_b in (x/5 for x in range(-5,5)):
         row = []
-        for c_b in (x/5 for x in range(-1,8)):
+        for c_a in (x/5 for x in range(-5,5)):
             row.append(julia_grid(c_a, c_b, width, height))
             print(f"calculated julia set with c = {c_a}, {c_b}")
-        julias.extend(row)
-        julias.extend(row[::-1])
 
+        if forwards:
+            julias.extend(row)
+        else:
+            julias.extend(row[::-1])
+        forwards = not forwards
     julias.extend(julias[::-1])
 
-    imageio.mimwrite('julia.gif', julias, duration=0.0001)
+    imageio.mimwrite('julia.gif', julias, duration=0.2)
 
 
 if __name__ == "__main__":
+    start = time.time()
     create_gif()
+    print(f"Time Taken:{time.time()-start}")
